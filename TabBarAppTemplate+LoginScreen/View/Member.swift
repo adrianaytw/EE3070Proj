a541ea9181bc9fb31cf7bf013fc7e7388f5bd204
AYTW <>
2021-03-31 15:13:40 +0800

    test

---
+++ TabBarAppTemplate+LoginScreen/View/Member.swift
@@ -0,0 +1,16 @@
+//
+//  Member.swift
+//  TabBarAppTemplate+LoginScreen
+//
+//  Created by Au Yeung Tsz Wang on 31/3/2021.
+//
+
+import Foundation
+
+
+class Member{
+    var memberfirstName : String?
+    var memberlastName : String?
+    var memberNumber : String?
+    
+}
---
+++ TabBarAppTemplate+LoginScreen/View/Product.swift
@@ -0,0 +1,19 @@
+//
+//  Product.swift
+//  TabBarAppTemplate+LoginScreen
+//
+//  Created by Au Yeung Tsz Wang on 31/3/2021.
+//
+
+import Foundation
+
+class Product{
+    var productName : String?
+    var remainquantity : Int = 0
+    var avaliable : Bool = false
+    var productCode : String?
+    var price : Float = 0
+    
+    
+}
+
